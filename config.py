3bffa3ed9631bc1069985ab8a64a5c92db8bd6ff
rik61072 <rik61072@gmail.com>
2020-02-04 23:30:39 +0530

    Updated Schema

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+from werkzeug.security import generate_password_hash as enc
+from werkzeug.security import check_password_hash as dec
+from hashlib import sha1
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask import Flask
@@ -25,21 +27,37 @@ class Thread(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     body = db.Column(db.String(500))
     img_ext = db.Column(db.String(10))
+    password = db.Column(db.String(500))
+    #pinned = db.Column(db.Integer)  #is it pinned or not? 
 
 class Post(db.Model):
     id = db.Column(db.Integer , unique=True , primary_key=True) #number, not the cookie id
     name = db.Column(db.String(50))
     img_name = db.Column(db.String(200) , default="")
     img_num = db.Column(db.Integer , default = 0 , unique = True)
-    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     body = db.Column(db.String(500))
     thread_id = db.Column(db.Integer)
     img_ext = db.Column(db.String(10))
+    password = db.Column(db.String(500))
+    options = db.Column(db.String(50)) #noko sage
 
 class Boards(db.model):
     name = db.Column(db.String(50))
     desc = db.Column(db.String(500))
+    last_id = db.Column(db.Integer) #to generate new id
 
+class User(db.model):
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(500))
+    email = db.Column(db.String(500))
+    #rank 0 = janny
+    #rank 1 = mod
+    #rank 2 = admin
+    rank = db.Column(db.Integer, default=0)
+    board_name = db.Column(db.String(50))
 
+def tripcodegen(thing):
+    return sha1(thing.encode("utf-8")).hexdigest()[:8]
 #Create the schema
 db.create_all()
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 from config import app
 
+
+
 @app.route("/")
 def index():
     return "welcome"
+
 
 if __name__ == "__main__":
     app.debug = True
