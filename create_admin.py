27d07380983135d92748610375fa51b246ffa56f
hrik2001 <rik61072@gmail.com>
2020-02-09 13:09:09 +0530

    updated ui

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 from main import *
 print("Initialising db")
 db.create_all()
-username=input("username for admin account")
-password=input("password for admin account")
+username=input("username for admin account ::")
+password=input("password for admin account ::")
 db.session.add(User(username=username , password=enc(password) , rank=2))
 db.session.commit()
